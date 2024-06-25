37bd2c08ba18473284727a23b41e96c957bca0c1
Luis Calvillo <luiscalvillo89@gmail.com>
2024-06-24 23:43:04 -0700

    Show table view stack views

--- Foodie/View/BusinessCell.swift
+++ Foodie/View/BusinessCell.swift
@@ -24,8 +24,8 @@ class BusinessCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         setupCellView()
-        setupStackView()
         setupInformationStackView()
+        setupStackView()
     }
     
     required init?(coder: NSCoder) {
@@ -35,6 +35,8 @@ class BusinessCell: UITableViewCell {
     private func setupCellView() {
         cellView.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(cellView)
+        
+        contentView.heightAnchor.constraint(equalToConstant: 150).isActive = true
         
         NSLayoutConstraint.activate([
             cellView.topAnchor.constraint(equalTo: contentView.topAnchor),
@@ -48,31 +50,39 @@ class BusinessCell: UITableViewCell {
         businessImageView.layer.cornerRadius = 25
         businessImageView.clipsToBounds = true
         businessImageView.translatesAutoresizingMaskIntoConstraints = false
-        businessImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        businessImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        businessImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        businessImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
         
         stackView = UIStackView(arrangedSubviews: [businessImageView, informationStackView])
         stackView.axis = .horizontal
         stackView.distribution = .fill
-        stackView.spacing = 10
-        stackView.alignment = .center
+        stackView.spacing = 8
+        stackView.alignment = .leading
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
         cellView.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: cellView.topAnchor, constant: 10),
-            stackView.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 10),
-            stackView.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -10),
-            stackView.bottomAnchor.constraint(equalTo: cellView.bottomAnchor, constant: -10)
+            stackView.topAnchor.constraint(equalTo: cellView.topAnchor, constant: 8),
+            stackView.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: 8),
+            stackView.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -8),
+            stackView.bottomAnchor.constraint(equalTo: cellView.bottomAnchor, constant: -8)
         ])
     }
     
     private func setupInformationStackView() {
+    
         informationStackView = UIStackView(arrangedSubviews: [nameLabel, addressLabel, distanceLabel])
         informationStackView.axis = .vertical
-        informationStackView.distribution = .fillEqually
+        informationStackView.distribution = .fill
         informationStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        nameLabel.textColor = .label
+        addressLabel.textColor = .label
+        distanceLabel.textColor = .label
+       
+        stackView.addSubview(informationStackView)
+    
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
